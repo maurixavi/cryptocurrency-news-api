0c179b6e1326414505cedea86f1e64032c03d696
maurixavi <mxavierarevalo@gmail.com>
2022-03-08 02:14:00 -0300

    more news sources added

--- index.js
+++ index.js
@@ -10,29 +10,122 @@ app.get('/',  (req, res) => {
     res.send('Cryptocurrency News API')
 })
 
+const sites = [
+    {
+        name: 'coindesk',
+        address: 'https://www.coindesk.com',
+        url_base: 'https://www.coindesk.com'
+    },
+    {
+        name: 'cointelegraph',
+        address: 'https://cointelegraph.com',
+        url_base: 'https://cointelegraph.com'
+    },
+    {
+        name: 'yahoo-finance',
+        address: 'https://finance.yahoo.com/topic/crypto',
+        url_base: 'https://finance.yahoo.com'
+    },
+    {
+        name: 'crypto-news',
+        address: 'https://cryptonews.com',
+        url_base: 'https://cryptonews.com'
+        
+    },
+    {
+        name: 'fortune',
+        address: 'https://fortune.com/advanced-search/?query=crypto',
+        url_base: 'https://fortune.com/advanced-search/?query=crypto'
+        
+    },
+    {
+        name: 'bloomberg',
+        address: 'https://www.bloomberg.com/crypto',
+        url_base: 'https://www.bloomberg.com'
+        
+    }
+]
+
 const articles = []
 
-app.get('/news', (req, res) => {
-    const url_base = 'https://www.coindesk.com/'
-    axios.get("https://www.coindesk.com")
+
+sites.forEach(site => {
+    axios.get(site.address)
         .then((response) => {
             const html = response.data
             
             const $ = cheerio.load(html)
 
+            //coindesk
             $('.headline', html).each(function() {
                 const title = $(this).text()
                 const url = $(this).attr('href')
+
                 articles.push({
                     title,
-                    url: url_base + url
+                    url: site.url_base + url,
+                    source: site.name
                 })
             })
-            res.json(articles)
+
+            //cointelegraph
+            $('.post-card__title-link', html).each(function() {
+                const title = $(this).text()
+                const url = $(this).attr('href')
+
+                articles.push({
+                    title,
+                    url: site.url_base + url,
+                    source: site.name
+                })
+            })
+
+            //yahoo finance
+            $('.js-content-viewer', html).each(function() {
+                const title = $(this).text()
+                const url = $(this).attr('href')
+                //console.log($(this).attr('href'));
+                console.log(site.url_base + url);
+                articles.push({
+                    title,
+                    url: site.url_base + url,
+                    source: site.name
+                })
+            })
+
+            //crypto news
+            $('.article__title article__title--lg article__title--featured  mb-20', html).each(function() {
+                const title = $(this).text()
+                const url = $(this).attr('href')
+
+                articles.push({
+                    title,
+                    url: site.url_base + url,
+                    source: site.name
+                })
+            })
+
+            //crypto news
+            $('.story-package-module__story__headline-link', html).each(function() {
+                
+                const title = $(this).text()
+                const url = $(this).attr('href')
+
+                articles.push({
+                    title,
+                    url: site.url_base + url,
+                    source: site.name
+                })
+            })
+            
+            
         }).catch((err) => console.log(err))
 
 })
 
+app.get('/news', (req, res) => {
+    res.json(articles)
+})
 
 
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`))
