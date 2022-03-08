422fe5208bbd7b7e938c2fedea2d1568913ee84b
maurixavi <mxavierarevalo@gmail.com>
2022-03-08 02:30:20 -0300

    headline prop added to news sites

--- index.js
+++ index.js
@@ -14,35 +14,38 @@ const sites = [
     {
         name: 'coindesk',
         address: 'https://www.coindesk.com',
-        url_base: 'https://www.coindesk.com'
+        url_base: 'https://www.coindesk.com',
+        headline_prop: '.headline'
     },
     {
         name: 'cointelegraph',
         address: 'https://cointelegraph.com',
-        url_base: 'https://cointelegraph.com'
+        url_base: 'https://cointelegraph.com',
+        headline_prop: '.post-card__title-link'
     },
     {
         name: 'yahoo-finance',
         address: 'https://finance.yahoo.com/topic/crypto',
-        url_base: 'https://finance.yahoo.com'
+        url_base: 'https://finance.yahoo.com',
+        headline_prop: '.js-content-viewer'
     },
     {
         name: 'crypto-news',
         address: 'https://cryptonews.com',
-        url_base: 'https://cryptonews.com'
-        
+        url_base: 'https://cryptonews.com',
+        headline_prop: '.story-package-module__story__headline-link'
     },
     {
         name: 'fortune',
         address: 'https://fortune.com/advanced-search/?query=crypto',
-        url_base: 'https://fortune.com/advanced-search/?query=crypto'
-        
+        url_base: 'https://fortune.com/advanced-search/?query=crypto',
+        headline_prop: '.queryly_item_title'
     },
     {
         name: 'bloomberg',
         address: 'https://www.bloomberg.com/crypto',
-        url_base: 'https://www.bloomberg.com'
-        
+        url_base: 'https://www.bloomberg.com',
+        headline_prop: '.story-package-module__story__headline-link'
     }
 ]
 
@@ -56,8 +59,7 @@ sites.forEach(site => {
             
             const $ = cheerio.load(html)
 
-            //coindesk
-            $('.headline', html).each(function() {
+            $(site.headline_prop, html).each(function() {
                 const title = $(this).text()
                 const url = $(this).attr('href')
 
@@ -68,56 +70,6 @@ sites.forEach(site => {
                 })
             })
 
-            //cointelegraph
-            $('.post-card__title-link', html).each(function() {
-                const title = $(this).text()
-                const url = $(this).attr('href')
-
-                articles.push({
-                    title,
-                    url: site.url_base + url,
-                    source: site.name
-                })
-            })
-
-            //yahoo finance
-            $('.js-content-viewer', html).each(function() {
-                const title = $(this).text()
-                const url = $(this).attr('href')
-                //console.log($(this).attr('href'));
-                console.log(site.url_base + url);
-                articles.push({
-                    title,
-                    url: site.url_base + url,
-                    source: site.name
-                })
-            })
-
-            //crypto news
-            $('.article__title article__title--lg article__title--featured  mb-20', html).each(function() {
-                const title = $(this).text()
-                const url = $(this).attr('href')
-
-                articles.push({
-                    title,
-                    url: site.url_base + url,
-                    source: site.name
-                })
-            })
-
-            //crypto news
-            $('.story-package-module__story__headline-link', html).each(function() {
-                
-                const title = $(this).text()
-                const url = $(this).attr('href')
-
-                articles.push({
-                    title,
-                    url: site.url_base + url,
-                    source: site.name
-                })
-            })
-            
             
         }).catch((err) => console.log(err))
 
